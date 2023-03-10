42e61ba7f06aac9b47041660eb72bf7f37841a3d
Ludovic Fournier <fouludovic@gmail.com>
2023-03-10 13:46:15 -0500

    q

--- src/route/Friend.route.js
+++ src/route/Friend.route.js
@@ -46,7 +46,7 @@ class FriendRoute {
         }
 
         try {
-            let result1 = await UserRepository.removeFriendReciveRequest(req.params.idUser, req.query.friendid);
+            let result1 = await UserRepository.removeFriendSendRequest(req.params.idUser, req.query.friendid);
             let result2 = await UserRepository.removeFriendReciveRequest(req.query.friendid, req.params.idUser);
             if (result1 === null || result2 === null) {
                 return next(HttpError.NotFound(`User ${req.params.idUser} dosen't exist`));
